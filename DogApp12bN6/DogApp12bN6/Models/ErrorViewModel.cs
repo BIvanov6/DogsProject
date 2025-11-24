b4c7179fdab32acac047f90d6a9475ad69990e17
“BIvanov6” <b.ivanov2021@pmg-pernik.com>
2025-11-24 09:04:42 +0200

    Upload Project

---
+++ DogApp12bN6/DogApp12bN6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DogApp12bN6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
